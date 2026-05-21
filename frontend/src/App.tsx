f1a32c6fcad13ceca11ea875480e2902715cb0bd
Aditya Shankar Lal <facc6982@gmail.com>
2026-05-21 15:40:02 +0530

    feat(frontend): implement dashboard pages, routes, and main application entry

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-
 function App() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-900 text-white">
-      <h1 className="text-4xl font-bold text-indigo-500 mb-2">CargoGo</h1>
-      <p className="text-slate-400 text-lg">Initial project setup completed successfully.</p>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-red-100">
+            <h1 className="text-9xl font-bold p-4">CargoGo</h1>
+            <h1 className="text-8xl font-bold">hello</h1>
+        </div>
+    );
 }
-
 export default App;
