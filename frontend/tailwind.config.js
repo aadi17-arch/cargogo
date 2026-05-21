530d22eaf7ebc3f0b273285060481b75409d3b97
Aditya Shankar Lal <facc6982@gmail.com>
2026-05-21 15:38:37 +0530

    chore: setup project-level base configuration and environment files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        brand: {
-          light: '#4f46e5',
-          DEFAULT: '#3730a3',
-          dark: '#1e1b4b',
-        }
-      }
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
+    plugins: [],
 }
