dcbde068bf91f55ba358b26ea026ea8f57cf9c27
Aditya Shankar Lal <facc6982@gmail.com>
2026-06-03 00:12:27 +0530

    frontend: update App routing and providers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter , Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ShipperDashboard from './pages/ShipperDashboard';
@@ -6,26 +6,36 @@ import DriverDashboard from './pages/DriverDashboard';
 import TrackingPage from './pages/TrackingPage';
 import Layout from './components/layout/Layout';
 import ProtectedRoute from './components/layout/ProtectedRoute';
+
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Navigate to="/login" replace />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
 
-                <Route element={<Layout />}>
-                    <Route element = {<ProtectedRoute/>}>
-                <Route path="/shipper" element={<ShipperDashboard/>} />
-                <Route path="/driver" element={<DriverDashboard/>} />
-                    <Route path="/track/:bookingId" element={<TrackingPage />} />
-                    </Route>
-                </Route>
-                <Route path = "*" element={<Navigate to ="/login"/>}></Route>
+        <Route element={<Layout />}>
+          {/* Shipper Dashboard Guard */}
+          <Route element={<ProtectedRoute allowedRoles={['SHIPPER']} />}>
+            <Route path="/shipper" element={<ShipperDashboard />} />
+          </Route>
 
-            </Routes>
-        </BrowserRouter>
-    );
+          {/* Driver Dashboard Guard */}
+          <Route element={<ProtectedRoute allowedRoles={['DRIVER']} />}>
+            <Route path="/driver" element={<DriverDashboard />} />
+          </Route>
+
+          {/* General Protected Routes (Any logged-in role can track) */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/track/:bookingId" element={<TrackingPage />} />
+          </Route>
+        </Route>
+
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
