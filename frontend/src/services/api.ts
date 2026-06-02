958d279a2ae3c3a2daef914bd5c9234564aedb0a
Aditya Shankar Lal <facc6982@gmail.com>
2026-06-03 00:12:28 +0530

    frontend: update api service client configuration

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
+export const BASE_URL = 'http://192.168.1.9:5000';
+export const SOCKET_URL = 'http://192.168.1.9:5000';
+
 const api = axios.create({
-  baseURL: `http://${window.location.hostname}:5000/api`,
+  baseURL: `${BASE_URL}/api`,
 });
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
 export default api;
