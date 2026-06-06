7fcd7ef578239ef79e583e3e3de9ac1fbf500431
Aditya Shankar Lal <facc6982@gmail.com>
2026-06-06 19:37:50 +0530

    feat(frontend): integrate and set up api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,16 +1,46 @@
 import axios from 'axios';
-
-export const BASE_URL = 'http://192.168.1.9:5000';
-export const SOCKET_URL = 'http://192.168.1.9:5000';
+import { API_URL } from '@/utils/constants';
 
 const api = axios.create({
-  baseURL: `${BASE_URL}/api`,
+  baseURL: API_URL,
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
-});
+},
+  (e) => {
+    return Promise.reject(e);
+  }
+);
+api.interceptors.response.use(
+  (res) => res,
+  async (e) => {
+    const originalRequest = e.config;
+
+    if (e.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const res = await axios.post(
+          `${API_URL}/auth/refresh`, {}, { withCredentials: true }
+        );
+        const { accessToken } = res.data.data;
+        localStorage.setItem('token', accessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+        return api(originalRequest);
+      }
+      catch (refreshError) {
+        localStorage.removeItem('token');
+        window.location.href = '/login';
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(e);
+  }
+);
 
 export default api;
