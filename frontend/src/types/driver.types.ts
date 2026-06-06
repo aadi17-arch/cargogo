ffbc6458c175f6a11ad6e9d2e505515da01f0a57
Aditya Shankar Lal <facc6982@gmail.com>
2026-06-06 19:37:51 +0530

    feat(frontend): integrate and set up driver.types

---
+++ frontend/src/types/driver.types.ts
@@ -0,0 +1,23 @@
+import { User } from './auth.types';
+import { Vehicle } from './vehicle.types';
+
+export interface DriverProfile {
+  id: string;
+  userId: string;
+  isOnline: boolean;
+  latitude: number | null;
+  longitude: number | null;
+  updatedAt: string;
+  user?: User;
+}
+
+export interface DriverStatusUpdate {
+  isOnline: boolean;
+  latitude?: number;
+  longitude?: number;
+}
+
+export interface DriverWithVehicle extends User {
+  driverProfile: DriverProfile | null;
+  vehicle: Vehicle | null;
+}
