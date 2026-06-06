406a0a8aa9407145aef496025de7b468bef3a44d
Aditya Shankar Lal <facc6982@gmail.com>
2026-06-06 19:37:50 +0530

    feat(frontend): integrate and set up driver.service

---
+++ frontend/src/services/driver.service.ts
@@ -0,0 +1,33 @@
+import api from './api';
+import { DriverProfile } from '../types/driver.types';
+import { Vehicle } from '../types/vehicle.types';
+import { ApiResponse } from '../types/api.types';
+
+export const driverService = {
+  async updateOnlineStatus(isOnline: boolean): Promise<DriverProfile> {
+    const response = await api.post<ApiResponse<DriverProfile>>('/drivers/online', { isOnline });
+    return response.data.data!;
+  },
+
+  async updateLocation(latitude: number, longitude: number): Promise<DriverProfile> {
+    const response = await api.post<ApiResponse<DriverProfile>>('/drivers/location', {
+      latitude,
+      longitude,
+    });
+    return response.data.data!;
+  },
+
+  async getMyVehicle(): Promise<Vehicle> {
+    const response = await api.get<ApiResponse<Vehicle>>('/vehicles/me');
+    return response.data.data!;
+  },
+
+  async registerOrUpdateVehicle(vehicleData: {
+    type: string;
+    plateNumber: string;
+    capacityKg: number;
+  }): Promise<Vehicle> {
+    const response = await api.put<ApiResponse<Vehicle>>('/vehicles/update', vehicleData);
+    return response.data.data!;
+  },
+};
