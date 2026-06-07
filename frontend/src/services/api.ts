9cbc2399d837dccaa111318c32edc6f0ced1db95
Aditya Shankar Lal <facc6982@gmail.com>
2026-06-07 16:35:27 +0530

    chore: update code for frontend/src/services/api.ts (30 of 44)

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -8,7 +8,7 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token && token !== 'undefined') config.headers.Authorization = `Bearer ${token}`;
   return config;
 },
   (e) => {
