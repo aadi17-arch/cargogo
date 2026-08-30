4b508d7dba1357c186b9d46dd876d1544eb26b1f
Aditya Shankar Lal <facc6982@gmail.com>
2026-08-30 16:07:54 +0530

    chore(cleanup): remove JSX comments from MapViewHelper.tsx

--- frontend/src/components/map/MapViewHelper.tsx
+++ frontend/src/components/map/MapViewHelper.tsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
 
-
 export function ChangeMapView({ center }: { center: [number, number] }) {
   const map = useMap();
   useEffect(() => {
@@ -10,7 +9,6 @@ export function ChangeMapView({ center }: { center: [number, number] }) {
   }, [center, map]);
   return null;
 }
-
 
 export function MapInstanceTracker({ setMap }: { setMap: (map: L.Map) => void }) {
   const map = useMap();
