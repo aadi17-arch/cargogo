67addcddbe24a1b61320591eccf9164b38fb4957
Aditya Shankar Lal <facc6982@gmail.com>
2026-08-30 15:06:25 +0530

    feat(refactor): update redis.ts

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,13 +1,9 @@
-import { createClient } from 'redis';
+import Redis from 'ioredis';
 import { env } from '@/config/env.config';
 
-export const redis = createClient({
-  url: env.REDIS_URL
+export const redis = new Redis(env.REDIS_URL, {
+  maxRetriesPerRequest: null,
 });
 
 redis.on('error', (err) => console.error('Redis Client Error', err));
-
-redis.connect().catch((err) => {
-  console.error('Failed to connect to Redis', err);
-});
 
